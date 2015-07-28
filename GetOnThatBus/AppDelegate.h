24a67ab1eb91a332bbf909b7db2872a6ff30ced2
Andrew Nguyen <anguy080@ucr.edu>
2015-07-28 13:57:39 -0700

    Initial Commit

---
+++ GetOnThatBus/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GetOnThatBus
+//
+//  Created by Andrew  Nguyen on 7/28/15.
+//  Copyright (c) 2015 Andrew Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
